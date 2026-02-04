10b31fca220e2abc4e1549410911c304d7523c5a
AlbertFrank-commins <albertfrankfg7a@gmail.com>
2026-02-04 17:01:56 -0400

    Se agrego la obtencion de eventos en js y se agrego el body y header en Css

---
+++ app.js
@@ -0,0 +1,17 @@
+// ======= ELEMENTOS DEL DOM =======
+const pokemonInput = document.getElementById("pokemonInput");
+const btnBuscar = document.getElementById("btnBuscar");
+
+const pokemonResultado = document.getElementById("pokemonResultado");
+const pokemonError = document.getElementById("pokemonError");
+
+const pokeImg = document.getElementById("pokeImg");
+const pokeNombre = document.getElementById("pokeNombre");
+const pokeTipos = document.getElementById("pokeTipos");
+const statHp = document.getElementById("statHp");
+const statAtk = document.getElementById("statAtk");
+const statDef = document.getElementById("statDef");
+
+const btnChiste = document.getElementById("btnChiste");
+const jokeText = document.getElementById("jokeText");
+const jokeError = document.getElementById("jokeError");
